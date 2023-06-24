28e45a69eabf52d0b636cf3466e621f28eaa01d1
Mr-Nobody <108734976+rahul-666@users.noreply.github.com>
2023-06-24 16:20:35 +0530

    Updated v2

--- JobSheet.py
+++ JobSheet.py
@@ -1,11 +1,12 @@
-import os,shutil
+import os,shutil,datetime
 
-source="C:\\Users\\Subhasish\\Desktop\\JOB SHEET\\JOB SHEET\\JOB SHEET.xlsx"
-source=source.replace("\\","/")
-destination="C:\\Users\\Subhasish\\Desktop\\JOB SHEET"
-destination=destination.replace("\\","/")
+#Properties
+destination="C:/Users/Subhasish/Desktop/JOB SHEET"
+backupLocation="D:/SUBHASISH/JOB SHEET"
 MainFile="JOB SHEET.xlsx"
+source=destination+"/JOB SHEET/"+MainFile
 NewFile=""
+
 #This Dictionary is used to store lot number as a key and file name as a value.
 Lots={}
 
@@ -20,6 +21,7 @@ def main():
             end=z.find("]")
             slnum=int(z[start:end])
             if x<slnum:x=slnum
+
     print(f"Last sl number was {x}, new one is {x+1}")
     serial_no=x+1
     lotNumber=input("Enter LotNumber->")
@@ -28,7 +30,17 @@ def main():
         shutil.copy(source,destination)
         NewFile=f"[{serial_no}] {lotNumber}.xlsx"
         os.rename(destination+"/"+MainFile,destination+"/"+NewFile)
+        
+        #To creat a logs of Every Creation
+        Logs(f"{NewFile} Created")
+
         os.startfile(destination+"/"+NewFile)
+        backupBool=input("Do you want to backup?(y/n)\n->>")
+        if input.lower()=="y":
+            Backup()
+            print("Backup sucessfull.")
+        else:
+            print("Closing without Backup!")
     else:
         print("Lot already Exists!",Lots[lotNumber])
 
@@ -51,5 +63,21 @@ def LotNum():
             startIndex=name.find("]")+2
             name=name[startIndex:]
             Lots[name]=z
+
+#Creating Logs is Always helpfull
+def Logs(FileName):
+    with open(f"{destination}/Logs/Logs.txt","a") as logs:
+        logs.write(f"{FileName} {datetime.datetime.now()}\n")
+        logs.close()
+
+#For Backup All data
+def Backup():
+        try:
+            shutil.rmtree(backupLocation)
+            shutil.copytree(destination,backupLocation)
+        except FileExistsError:pass
+        except Exception as e:
+            print(e)
+
 
 main()
